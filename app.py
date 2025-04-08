b1f8492deb54543e204e9d1ef353b558c57befd8
KristinSchanks <kristinschanks@gmail.com>
2025-04-08 10:08:15 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Healthcare Partners QMS is running!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
